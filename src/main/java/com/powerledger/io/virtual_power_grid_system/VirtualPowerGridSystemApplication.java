0ed2b537c638a27291aec0ecf2c7ed284df41047
shshetudev <shshetudev@gmail.com>
2025-05-08 21:13:17 +0600

    feat(project-initialization): Added initial project structure

---
+++ src/main/java/com/powerledger/io/virtual_power_grid_system/VirtualPowerGridSystemApplication.java
@@ -0,0 +1,13 @@
+package com.powerledger.io.virtual_power_grid_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualPowerGridSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualPowerGridSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/powerledger/io/virtual_power_grid_system/VirtualPowerGridSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.powerledger.io.virtual_power_grid_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualPowerGridSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
